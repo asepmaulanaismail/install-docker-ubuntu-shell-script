4ee9e9230e2dd3e98c277c508bb6a56b7cae3560
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-01-17 18:57:58 -0800

    Adding install docker-compose

--- docker-install.sh
+++ docker-install.sh
@@ -13,7 +13,7 @@ sudo add-apt-repository \
     main"
 
 sudo apt-get update
-sudo apt-get -y install docker-engine
+sudo apt-get -y install docker-engine docker-compose
 
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
